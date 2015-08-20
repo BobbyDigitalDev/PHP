22185bf07ba9477dfc353f8ff0d8b12fdfaa89b4
BobbyDigitalDev <bobbyl76@gmail.com>
2015-08-20 19:08:27 -0400

    functions

--- index.php
+++ index.php
@@ -6,13 +6,28 @@
 </head>
 <body>
 
-<!-- functions -->
+<!-- PHP functions -->
+
+<!-- strlen function gives you the length of a string -->
 <?php 
   $length = strlen("bobby");
   print $length;
- ?>
+  ?>
 
- <!-- strlen gives you the length of a string -->
+
+<!-- substr function allows you to select 2 array points from which to slice out a portion of a string -->
+<?php
+  $myname = "Bobby";
+  $partial = substr($myname, 2, 4);
+  print $partial;
+  ?>
+
+
+<!-- strtoupper function takes a variable and makes it uppercase -->
+<?php 
+  $uppercase = strtoupper($myname);
+  print $uppercase;
+  ?>
   
 </body>
 </html>
